1cbafd31a6cacbca07f973a40642d243e79121ce
sercas <sergiocupe@gmail.com>
2023-09-09 20:37:54 -0300

    Quitar archivos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Header from './componentes/Header/Header';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import Footer from './componentes/Footer/Footer';
